4f8b3632d98719aa8bfa967755e200961fd5d60c
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2019-03-06 22:21:30 +0800

    Added the 5% prevalance filter for single biomes and ran them

--- Single_biome_clustering/snf.R
+++ Single_biome_clustering/snf.R
@@ -6,12 +6,21 @@ b_data=read.csv("./../Data/bacteria.csv",row.names = 1)
 f_data=read.csv("./../Data/fungi.csv",row.names = 1)
 v_data=read.csv("./../Data/virus.csv",row.names = 1)
 
-v_a<-colSums(v_data)
-for (i in colnames(v_data)){
-  if (v_a[[i]]==0){v_data[[i]]<-NULL}
+#Filter and finding the weights
+list_sel=list()
+count=1
+for (d in list(b_data,f_data,v_data)){
+  z=colSums(d>0)
+  sel_col=row.names(as.data.frame(z[z>=10])) #In 5% patients prevalent
+  #print(sel_col)
+  list_sel[[count]]<-sel_col
+  count=count+1
 }
 
-v_data<-v_data[,c("PIV3","PIV4","RV","Boca")] #Selected viruses after prevalance filter for atleast 5 patients
+b_data<-b_data[,list_sel[[1]]]
+f_data<-f_data[,list_sel[[2]]]
+v_data<-v_data[,list_sel[[3]]]
+remove(d,list_sel,count,sel_col,z)
 
 b_dsim=vegdist(b_data,method='bray',diag=TRUE,upper=TRUE)
 f_dsim=vegdist(f_data,method='bray',diag=TRUE,upper=TRUE)
