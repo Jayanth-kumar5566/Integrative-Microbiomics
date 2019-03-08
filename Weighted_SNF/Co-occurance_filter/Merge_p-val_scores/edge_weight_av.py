d8e360816876c06f8e7fc8f932b52f6c70de405a
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2019-03-08 09:11:11 +0800

    Added correction for summing edge weights

--- Weighted_SNF/Co-occurance_filter/Merge_p-val_scores/edge_weight_av.py
+++ Weighted_SNF/Co-occurance_filter/Merge_p-val_scores/edge_weight_av.py
@@ -3,6 +3,9 @@ import pandas
 cl1=pandas.read_csv("cluster1_Adj_cyto.csv",index_col=0)
 cl2=pandas.read_csv("cluster2_Adj_cyto.csv",index_col=0)
 
+cl1=cl1.abs()
+cl2=cl2.abs()
+
 cl1=cl1.sum(axis=1)
 cl2=cl2.sum(axis=1)
 
