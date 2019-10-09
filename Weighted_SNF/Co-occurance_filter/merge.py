ca3718b642ed06bf435b9d6ebeb22ac525688f84
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2019-10-09 23:57:41 +0800

    Modified renormalization in Filter Co-occurance

--- Weighted_SNF/Co-occurance_filter/merge.py
+++ Weighted_SNF/Co-occurance_filter/merge.py
@@ -14,18 +14,20 @@ y=x.join(vir,how="inner")
 
 y=y.div(y.sum(axis=1), axis=0) #Renormalize
 
-df=y.join(lab,how="inner")
-
 #Dropping 0 columns
-nsel=df.sum(axis=0)
+nsel=y.sum(axis=0)
 n=nsel[nsel==0].index
-df.drop(columns=n,inplace=True)
+y.drop(columns=n,inplace=True)
 
 #Non-zero value in atleast 10 patients
-nsel=df>0.01
+nsel=y>0.01
 nsel=nsel.sum(axis=0)
-y=nsel[nsel<10].index #5%
-df.drop(columns=y,inplace=True)
+x=nsel[nsel<10].index #5%
+y.drop(columns=x,inplace=True)
+
+y=y.div(y.sum(axis=1), axis=0) #Renormalize
+
+df=y.join(lab,how="inner")
 
 df.rename(columns={"labels":"x"},inplace=True)
 df.to_csv("Microbes.csv")
